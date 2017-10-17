f9bf52356c2a1f6832d0a0dff88ddde0dae69004
Şafak Çıplak <safakciplak1990@gmail.com>
2017-10-18 01:07:37 +0300

    tooltip example

--- resources/views/files.blade.php
+++ resources/views/files.blade.php
@@ -3,44 +3,64 @@
     <input type="submit" value="Search"/>
 </form>
 
+
+
 <style>
-    .tooltip span {
-        display: none;
+    .tooltip {
+        position: relative;
+        display: inline-block;
+        border-bottom: 1px dotted black;
     }
 
-    .tooltip:hover span {
-        display: block;
-        position: fixed;
-        overflow: hidden;
+    .tooltip .tooltiptext {
+        visibility: hidden;
+        width: 120px;
+        background-color: #555;
+        color: #fff;
+        text-align: center;
+        border-radius: 6px;
+        padding: 5px 0;
+        position: absolute;
+        z-index: 1;
+        bottom: 125%;
+        left: 50%;
+        margin-left: -60px;
+        opacity: 0;
+        transition: opacity 1s;
     }
+
+    .tooltip .tooltiptext::after {
+        content: "";
+        position: absolute;
+        top: 100%;
+        left: 50%;
+        margin-left: -5px;
+        border-width: 5px;
+        border-style: solid;
+        border-color: #555 transparent transparent transparent;
+    }
+
+    /*.tooltip:hover .tooltiptext {*/
+        /*visibility: visible;*/
+        /*opacity: 1;*/
+    /*}*/
 </style>
 
-
-<div title="regular tooltip">Hover me<span style="display: none" id="tooltip-span">safak</span></div>
+<div class="tooltip" id="test">Hover over me
+    <span class="tooltiptext">Tooltip text</span>
+</div>
 
 <script>
-    var tooltipSpan = document.getElementById('tooltip-span');
-    window.onmousemove = function (e) {
-        var x = e.clientX,
-                y = e.clientY;
-        tooltipSpan.style.top = (y + 20) + 'px';
-        tooltipSpan.style.left = (x + 20) + 'px';
-    };
+    var test = document.getElementById("test");
+    // this handler will be executed only once when the cursor moves over the unordered list
+    test.addEventListener("mouseover", function( event ) {
+        document.getElementsByClassName("tooltiptext")[0].style.visibility = "visible";
+        document.getElementsByClassName("tooltiptext")[0].style.opacity = 1;
+    }, false);
 
-//    var tooltips = document.querySelectorAll('.tooltip span');
-//
-//    window.onmousemove = function (e) {
-//        var x = e.clientX,
-//                y = e.clientY;
-//        for (var i = 0; i < tooltips.length; i++) {
-//            tooltips[i].style.top = y;
-//            tooltips[i].style.left = x;
-//        }
-//
-//    }
+    test.addEventListener("mouseleave", function( event ) {
+        document.getElementsByClassName("tooltiptext")[0].style.visibility = "hidden";
+        document.getElementsByClassName("tooltiptext")[0].style.opacity = 0;
+    }, false);
+
 </script>
-
-{{--<h1 rel="safak tooltip">naber</h1>--}}
-{{--<script>--}}
-{{--var x = document.getElementsByTagName("h1")[0].getAttribute("rel");--}}
-{{--</script>--}}
